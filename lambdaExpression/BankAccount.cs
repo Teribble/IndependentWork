5237aa4098f30d11c7c09f0978cd25f0bd5e227a
Евгений Гергелиджиу <Euphor1aVegas@gmail.com>
2022-07-10 13:50:45 +0300

    11

---
+++ lambdaExpression/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿namespace lambdaExpression
+{
+    public class BankAccount
+    {
+        public delegate void BankAccountDel(string message);
+        public event BankAccountDel? Notify;
+        public decimal Remiander { get; set; }
+
+        public string? Name { get; set; }
+
+        public BankAccount(string? name)
+        {
+            Name = name;
+
+            Remiander = 0;
+        }
+
+        public void TopUpAccount(decimal amount)
+        {
+            Remiander += amount;
+
+            Notify?.Invoke($"Счет {this.Name} пополнен на {amount}$");
+            CurrentRemiander();
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Remiander > amount)
+            {
+                Remiander -= amount;
+                Notify?.Invoke($"Со счета {this.Name} снято {amount}$");
+            }
+            else
+            {
+                Notify?.Invoke($"Недостаточно средств на счету");
+                CurrentRemiander();
+            }
+
+
+        }
+
+        private void CurrentRemiander()
+        {
+            Notify?.Invoke($"Остаток по счету {this.Name}: {this.Remiander}$");
+        }
+    }
+}
---
+++ lambdaExpression/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lambdaExpression
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("BD-145");
+            ba.Notify += delegate (string? message) { Console.WriteLine(message); };
+            ba.Notify += Display;
+            ba.TopUpAccount(1000);
+            ba.Withdraw(50000);
+        }
+
+        public static void Display(string? meassage)=>Console.WriteLine("lamda" + "\n" + meassage);
+    }
+}
