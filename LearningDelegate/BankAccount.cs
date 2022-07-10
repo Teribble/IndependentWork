63e908866a548dc495aca4ec97419a24c9a4a3a4
Евгений Гергелиджиу <Euphor1aVegas@gmail.com>
2022-07-10 12:46:03 +0300

    Добавьте файлы проекта.

---
+++ LearningDelegate/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿namespace LearningDelegate
+{
+    public class BankAccount
+    {
+        public delegate void BankAccountDel(string message);
+        public BankAccountDel? BankAccountHandler;
+        public decimal Remiander { get; set; }
+
+        public string? Name { get; set; }
+
+        public BankAccount(string? name)
+        {
+            Name = name;
+
+            Remiander = 0;
+        }
+        public void RegisterDelegate(BankAccountDel del)
+        {
+            BankAccountHandler = del;
+        }
+
+        public void TopUpAccount(decimal amount)
+        {
+            Remiander += amount;
+
+            BankAccountHandler?.Invoke($"Счет {this.Name} пополнен на {amount}$");
+            CurrentRemiander();
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Remiander > amount)
+                Remiander -= amount;
+
+            BankAccountHandler?.Invoke($"Со счета {this.Name} снято {amount}$");
+            CurrentRemiander();
+        }
+
+        private void CurrentRemiander()
+        {
+            BankAccountHandler?.Invoke($"Остаток по счету {this.Name}: {this.Remiander}$");
+        }
+    }
+}
---
+++ LearningDelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LearningDelegate
+{
+    public class Program
+    {
+        public static void Print(string? message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void Main()
+        {
+            BankAccount acc = new BankAccount("DDMB-144687");
+            acc.RegisterDelegate(Print);
+            acc.TopUpAccount(34567);
+            acc.Withdraw(18000);
+        }
+    }
+}
